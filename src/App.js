6c38c620f48bd78c3dc613ef2591afc55affb9e4
Andrea Baglioni <andreabaglioni@sf-mbp-abaglioni.home>
2021-01-02 13:29:10 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Giorgio is a homosexual
+        </p>
+        <p>Giorgio is really a homo</p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
