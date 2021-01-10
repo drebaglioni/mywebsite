b61b5540204871fee385f626c98dd6ca682bce8f
Andrea Baglioni <andreabaglioni@sf-mbp-abaglioni.home>
2021-01-10 23:01:02 +0000

    all homepage first draft visuals

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,19 +1,68 @@
 import React, {Component} from "react"
+import "./pages.css"
+import dre from "./dre.png"
+import {NavLink} from "react-router-dom"
+
+
 class Homepage extends Component {
-    render () {
-        return(
-            <h1>Dre's Homepage</h1>
+    render() {
+        return (
+            <>
+
+
+                <div className="col-12 wrapper" id="topWrapper">
+                    <div className="row">
+
+
+                        <div className="col-10 mainDivs" id="leftDiv">
+
+                            <h1 className="mainTitle">andrea baglioni</h1>
+
+                        </div>
+                        <div className="col-2 mainDivs" id="rightDiv">
+
+                            <img src={dre}></img>
+
+                        </div>
+
+                    </div>
+                </div>
+
+
+                <div className="col-12 wrapper" id="topWrapper">
+                    <div className="row">
+
+
+                        <div className="col-7 mainDivs" id="thirdDiv">
+
+                            <img src={dre}></img>
+
+                        </div>
+                        <div className="col-5 mainDivs" id="fourthDiv">
+
+                            <NavLink className="navHome" to="/resume">
+                                Resume
+                            </NavLink>
+                            <br></br>
+                            <NavLink className="navHome" to="/contact">
+                                Contact Me
+                            </NavLink>
+                            <br></br>
+                            <NavLink className="navHome" to="/about">
+                                About
+                            </NavLink>
+
+                        </div>
+
+                    </div>
+                </div>
+
+            </>
         )
-    
-    
-    
-    
+
+
     }
 
 
-
-
-
 }
- export default Homepage
-
+export default Homepage
