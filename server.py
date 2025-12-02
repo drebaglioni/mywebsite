c2d4ff7a5868b2a62b524b32a59f0a5d35400a6f
drebaglioni <57418452+drebaglioni@users.noreply.github.com>
2025-12-01 18:06:13 -0800

    Add files via upload
    bahuas v1

---
+++ server.py
@@ -0,0 +1,15 @@
+import http.server
+import socketserver
+
+PORT = 5000
+
+class NoCacheHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
+    def end_headers(self):
+        self.send_header('Cache-Control', 'no-cache, no-store, must-revalidate')
+        self.send_header('Pragma', 'no-cache')
+        self.send_header('Expires', '0')
+        super().end_headers()
+
+with socketserver.TCPServer(("0.0.0.0", PORT), NoCacheHTTPRequestHandler) as httpd:
+    print(f"Serving at http://0.0.0.0:{PORT}")
+    httpd.serve_forever()
